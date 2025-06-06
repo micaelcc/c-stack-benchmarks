cbb51fe99008421e36257eb98772fa92d1ba087b
Micael Lima <50875854+micaelcc@users.noreply.github.com>
2025-06-06 18:45:09 -0300

    add main

---
+++ main.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <glib.h>
+
+typedef struct No
+{
+    int dado;
+    struct No *prox;
+} No;
+
+typedef struct
+{
+    No *topo;
+} PilhaEnc;
+
+PilhaEnc *criarPilhaEnc()
+{
+    PilhaEnc *p = malloc(sizeof(PilhaEnc));
+    p->topo = NULL;
+    return p;
+}
+
+static inline void pushEnc(PilhaEnc *restrict p, int valor)
+{
+    No *novo = malloc(sizeof(No));
+    novo->dado = valor;
+    novo->prox = p->topo;
+    p->topo = novo;
+}
+
+static inline int popEnc(PilhaEnc *restrict p)
+{
+    No *temp = p->topo;
+    int valor = temp->dado;
+    p->topo = temp->prox;
+    free(temp);
+    return valor;
+}
+
+typedef struct
+{
+    int *dados;
+    int topo;
+    int capacidade;
+} PilhaArr;
+
+PilhaArr *criarPilhaArr(int cap)
+{
+    PilhaArr *p = malloc(sizeof(PilhaArr));
+    p->dados = malloc(cap * sizeof(int));
+    p->topo = -1;
+    p->capacidade = cap;
+    return p;
+}
+
+static inline void redimensionar(PilhaArr *restrict p, int novo_cap)
+{
+    p->dados = realloc(p->dados, novo_cap * sizeof(int));
+    p->capacidade = novo_cap;
+}
+
+static inline void pushArr(PilhaArr *restrict p, int valor)
+{
+    if (++p->topo == p->capacidade)
+        redimensionar(p, p->capacidade * 2);
+    p->dados[p->topo] = valor;
+}
+
+static inline int popArr(PilhaArr *restrict p)
+{
+    return p->dados[p->topo--];
+}
+
+const int size = 100000000;
+
+__attribute__((hot)) void testEnc(PilhaEnc *restrict p)
+{
+    for (int i = 0; i < size; i++)
+        pushEnc(p, i);
+    for (int i = 0; i < size; i++)
+        popEnc(p);
+}
+
+__attribute__((hot)) void testArr(PilhaArr *restrict p)
+{
+    for (int i = 0; i < size; i++)
+        pushArr(p, i);
+    for (int i = 0; i < size; i++)
+        popArr(p);
+}
+
+__attribute__((hot)) void testGlib(GQueue *restrict g)
+{
+    for (int i = 0; i < size; i++)
+        g_queue_push_head(g, GINT_TO_POINTER(i));
+    for (int i = 0; i < size; i++)
+        g_queue_pop_head(g);
+}
+
+void main()
+{
+    clock_t t1, t2, t3, t4, t5, t6;
+    PilhaEnc *pEnc = criarPilhaEnc();
+    PilhaArr *pArr = criarPilhaArr(16);
+    GQueue *g = g_queue_new();
+
+    t1 = clock();
+    testEnc(pEnc);
+    t2 = clock();
+
+    t3 = clock();
+    testArr(pArr);
+    t4 = clock();
+
+    t5 = clock();
+    testGlib(g);
+    t6 = clock();
+
+    printf("Items pushed: 100 million\nItems popped: 100 million\n", size);
+    printf("linked list: %.3f s\n", (double)(t2 - t1) / CLOCKS_PER_SEC);
+    printf("      array: %.3f s\n", (double)(t4 - t3) / CLOCKS_PER_SEC);
+    printf("      glib:  %.3f s\n", (double)(t6 - t5) / CLOCKS_PER_SEC);
+
+    free(pArr->dados);
+    free(pArr);
+    while (pEnc->topo)
+        popEnc(pEnc);
+    free(pEnc);
+    g_queue_free(g);
+}
